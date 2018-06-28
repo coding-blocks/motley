eb4be5c0307e432ee02071a5e5a37afe135b56e0
Arnav Gupta <arnav@codingblocks.com>
2018-06-29 03:35:42 +0530

    clean live folder too
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- gruntfile.js
+++ gruntfile.js
@@ -93,6 +93,7 @@ module.exports = function(grunt) {
         },
         clean: [
             'dist',
+            'live',
             'examples/html',
             'examples/public/stylesheets/'
         ]
