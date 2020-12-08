cdff8b93aca36d31f5471465d1b4ee85be0e34d3
BipinKalra <bipinkalra@gmail.com>
2020-12-08 15:44:33 +0530

    minifix

--- examples/public/JS/radial-progress.js
+++ examples/public/JS/radial-progress.js
@@ -1,13 +1,21 @@
 var forEach = function (array, callback, scope) {
-	for (var i = 0; i < array.length; i++) {
-		callback.call(scope, i, array[i]);
-	}
+  for (var i = 0; i < array.length; i++) {
+    callback.call(scope, i, array[i]);
+  }
 };
 
-window.onload = function(){
-	var max = -219.99078369140625;
-	forEach(document.querySelectorAll('.radial-progress'), function (index, value) {
-	percent = value.getAttribute('data-progress');
-		value.querySelector('.fill').setAttribute('style', 'stroke-dashoffset: ' + ((100 - percent) / 100) * max);
-	});
-}
+window.onload = function () {
+  var max = -219.99078369140625;
+  forEach(
+    document.querySelectorAll(".radial-progress"),
+    function (index, value) {
+      percent = value.getAttribute("data-progress");
+      value
+        .querySelector(".fill")
+        .setAttribute(
+          "style",
+          "stroke-dashoffset: " + ((100 - percent) / 100) * max
+        );
+    }
+  );
+};
