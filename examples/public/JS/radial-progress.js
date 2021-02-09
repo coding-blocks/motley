068d2d97d684bd092abfcfbf3b9b18a6c9764b75
BipinKalra <bipinkalra@gmail.com>
2021-02-09 14:37:16 +0530

    lightify main page

--- examples/public/JS/radial-progress.js
+++ examples/public/JS/radial-progress.js
@@ -4,23 +4,7 @@ var forEach = function (array, callback, scope) {
   }
 };
 
-document.addEventListener("DOMContentLoaded", function (event) {
-  var max = -219.99078369140625;
-  forEach(
-    document.querySelectorAll(".radial-progress"),
-    function (index, value) {
-      percent = value.getAttribute("data-progress");
-      value
-        .querySelector(".fill")
-        .setAttribute(
-          "style",
-          "stroke-dashoffset: " + ((100 - percent) / 100) * max
-        );
-    }
-  );
-});
-
-// window.onload = function () {
+// document.addEventListener("DOMContentLoaded", function (event) {
 //   var max = -219.99078369140625;
 //   forEach(
 //     document.querySelectorAll(".radial-progress"),
@@ -34,4 +18,20 @@ document.addEventListener("DOMContentLoaded", function (event) {
 //         );
 //     }
 //   );
-// };
+// });
+
+window.onload = function () {
+  var max = -219.99078369140625;
+  forEach(
+    document.querySelectorAll(".radial-progress"),
+    function (index, value) {
+      percent = value.getAttribute("data-progress");
+      value
+        .querySelector(".fill")
+        .setAttribute(
+          "style",
+          "stroke-dashoffset: " + ((100 - percent) / 100) * max
+        );
+    }
+  );
+};
--- examples/public/JS/v2.js
+++ examples/public/JS/v2.js
@@ -1,34 +1,4 @@
-// window.onload = function () {
-//   document.querySelector("#mega-nav-toggle").addEventListener("click", () => {
-//     const meganav = document.querySelector("#mega-nav");
-
-//     if (meganav.classList.contains("d-none")) {
-//       meganav.classList.remove("d-none");
-//     } else {
-//       meganav.classList.add("d-none");
-//     }
-//   });
-
-//   document.querySelectorAll(".universe").forEach((universe) => {
-//     let reduceAngle = 60;
-//     universe.querySelectorAll(".universe__orbit").forEach((orbit) => {
-//       let planets = orbit.querySelectorAll(".universe__orbit__planet");
-//       let angle = 360 - 90;
-//       let dangle = 360 / planets.length;
-//       for (let i = 0; i < planets.length; ++i) {
-//         let planet = planets[i];
-//         angle += dangle;
-//         planet.style.transform = `rotate(${angle - reduceAngle}deg) translate(${
-//           orbit.clientWidth / 2
-//         }px) rotate(-${angle - reduceAngle}deg)`;
-//       }
-
-//       reduceAngle += 45;
-//     });
-//   });
-// };
-
-document.addEventListener("DOMConentLoaded", function (event) {
+window.onload = function () {
   document.querySelector("#mega-nav-toggle").addEventListener("click", () => {
     const meganav = document.querySelector("#mega-nav");
 
@@ -38,4 +8,34 @@ document.addEventListener("DOMConentLoaded", function (event) {
       meganav.classList.add("d-none");
     }
   });
-});
+
+  document.querySelectorAll(".universe").forEach((universe) => {
+    let reduceAngle = 60;
+    universe.querySelectorAll(".universe__orbit").forEach((orbit) => {
+      let planets = orbit.querySelectorAll(".universe__orbit__planet");
+      let angle = 360 - 90;
+      let dangle = 360 / planets.length;
+      for (let i = 0; i < planets.length; ++i) {
+        let planet = planets[i];
+        angle += dangle;
+        planet.style.transform = `rotate(${angle - reduceAngle}deg) translate(${
+          orbit.clientWidth / 2
+        }px) rotate(-${angle - reduceAngle}deg)`;
+      }
+
+      reduceAngle += 45;
+    });
+  });
+};
+
+// document.addEventListener("DOMConentLoaded", function (event) {
+//   document.querySelector("#mega-nav-toggle").addEventListener("click", () => {
+//     const meganav = document.querySelector("#mega-nav");
+
+//     if (meganav.classList.contains("d-none")) {
+//       meganav.classList.remove("d-none");
+//     } else {
+//       meganav.classList.add("d-none");
+//     }
+//   });
+// });
