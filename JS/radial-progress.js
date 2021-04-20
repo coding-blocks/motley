6b338733f8940086cecdc3f40e6ab2531e3a17f2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-04-20 12:02:18 +0000

    Deploy coding-blocks/motley to github.com/coding-blocks/motley.git:gh-pages

---
+++ JS/radial-progress.js
@@ -0,0 +1,37 @@
+var forEach = function (array, callback, scope) {
+  for (var i = 0; i < array.length; i++) {
+    callback.call(scope, i, array[i]);
+  }
+};
+
+// document.addEventListener("DOMContentLoaded", function (event) {
+//   var max = -219.99078369140625;
+//   forEach(
+//     document.querySelectorAll(".radial-progress"),
+//     function (index, value) {
+//       percent = value.getAttribute("data-progress");
+//       value
+//         .querySelector(".fill")
+//         .setAttribute(
+//           "style",
+//           "stroke-dashoffset: " + ((100 - percent) / 100) * max
+//         );
+//     }
+//   );
+// });
+
+window.onload = function () {
+  var max = -219.99078369140625;
+  forEach(
+    document.querySelectorAll(".radial-progress"),
+    function (index, value) {
+      percent = value.getAttribute("data-progress");
+      value
+        .querySelector(".fill")
+        .setAttribute(
+          "style",
+          "stroke-dashoffset: " + ((100 - percent) / 100) * max
+        );
+    }
+  );
+};
---
+++ JS/v2.js
@@ -0,0 +1,64 @@
+window.onload = function () {
+  // document.querySelector("#mega-nav-toggle").addEventListener("click", () => {
+  //   const meganav = document.querySelector("#mega-nav");
+
+  //   if (meganav.classList.contains("d-none")) {
+  //     meganav.classList.remove("d-none");
+  //   } else {
+  //     meganav.classList.add("d-none");
+  //   }
+  // });
+
+  // document.querySelectorAll(".universe").forEach((universe) => {
+  //   let reduceAngle = 60;
+  //   universe.querySelectorAll(".universe__orbit").forEach((orbit) => {
+  //     let planets = orbit.querySelectorAll(".universe__orbit__planet");
+  //     let angle = 360 - 90;
+  //     let dangle = 360 / planets.length;
+  //     for (let i = 0; i < planets.length; ++i) {
+  //       let planet = planets[i];
+  //       angle += dangle;
+  //       planet.style.transform = `rotate(${angle - reduceAngle}deg) translate(${
+  //         orbit.clientWidth / 2
+  //       }px) rotate(-${angle - reduceAngle}deg)`;
+  //     }
+
+  //     reduceAngle += 45;
+  //   });
+  // });
+
+  // const leftButton = document.querySelector("#move-left");
+  // const rightButton = document.querySelector("#move-right");
+  // const carousel = document.querySelector("#carousel");
+
+  // leftButton.addEventListener("click", () => {
+  //   carousel.scrollLeft += carousel.clientWidth;
+  // });
+
+  // rightButton.addEventListener("click", () => {
+  //   carousel.scrollLeft -= carousel.clientWidth;
+  // });
+
+  const multipleEntriesArray = ["Pitampura", "Noida", "Live"];
+  let entryCounter = 0;
+
+  const multipleEntryToggle = document.querySelector("#multiple-entry-toggle");
+
+  multipleEntryToggle.addEventListener("click", () => {
+    multipleEntryToggle.innerHTML = multipleEntriesArray[entryCounter++];
+
+    if ((entryCounter = multipleEntriesArray.length - 1)) entryCounter = 0;
+  });
+};
+
+// document.addEventListener("DOMConentLoaded", function (event) {
+//   document.querySelector("#mega-nav-toggle").addEventListener("click", () => {
+//     const meganav = document.querySelector("#mega-nav");
+
+//     if (meganav.classList.contains("d-none")) {
+//       meganav.classList.remove("d-none");
+//     } else {
+//       meganav.classList.add("d-none");
+//     }
+//   });
+// });
