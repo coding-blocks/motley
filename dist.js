8ac7c91a5ce1aa1f0fb38821040d1a47d53b07b8
Vibhu Dujari <vdvibhu20@gmail.com>
2021-08-16 23:43:59 +0530

    add scss files to dist

--- dist.js
+++ dist.js
@@ -2,21 +2,21 @@ const sass = require('node-sass');
 const config = require('./motley.config.json');
 const fs = require('fs')
 
-const writeToDisc = function (result, outFile){
-  fs.writeFileSync(outFile, result.css.toString(), {recursive: true})
+const writeToDisc = function (result, outFileCss){
+  fs.writeFileSync(outFileCss, result.css.toString(), {recursive: true})
 }
 
 config.applications.map(app => {
   const file = `sass/styles/applications/${app}/app.scss`
-  const outFile = `dist/${app}/app.css`
-  const outFileMin = `dist/${app}/app.min.css`
+  const outFileCss = `dist/${app}/app.css`
+  const outFileScss = `dist/${app}/scss/app.scss`
+  const outFileCssMin = `dist/${app}/app.min.css`
 
-  if (!fs.existsSync(`dist/${app}`)){
-    fs.mkdirSync(`dist/${app}`, { recursive: true })
-  }
+  fs.mkdirSync(`dist/${app}/scss`, { recursive: true })
 
-  writeToDisc(sass.renderSync({file}), outFile)
-  writeToDisc(sass.renderSync({file, outputStyle: 'compressed'}), outFileMin)
+  writeToDisc(sass.renderSync({file}), outFileCss)
+  writeToDisc(sass.renderSync({file, outputStyle: 'compressed'}), outFileCssMin)
+  fs.writeFileSync(outFileScss, fs.readFileSync(file, 'utf-8'))
 })
 
 writeToDisc(sass.renderSync({file: 'sass/styles/app.scss'}), 'dist/app.css')
